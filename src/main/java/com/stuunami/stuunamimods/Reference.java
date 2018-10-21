a59e827dbbea9b05d90d560f87ea97a0405bbe7c
Stuunami <Stuunami@gmail.com>
2018-10-21 16:53:33 -0400

    setup proxy structure

---
+++ src/main/java/com/stuunami/stuunamimods/Reference.java
@@ -0,0 +1,10 @@
+package com.stuunami.stuunamimods;
+
+public class Reference {
+    public static final String MODID = "stuunamimods";
+    public static final String NAME = "Stuunami Mods";
+    public static final String VERSION = "1.0";
+    public static final String MC_VERSION = "[1.12.2]";
+    public static final String CLIENT = "com.stuunami.stuunamimods.proxy.ClientProxy";
+    public static final String SERVER = "com.stuunami.stuunamimods.proxy.ServerProxy";
+}
--- src/main/java/com/stuunami/stuunamimods/StuunamiMods.java
+++ src/main/java/com/stuunami/stuunamimods/StuunamiMods.java
@@ -1,7 +1,8 @@
 package com.stuunami.stuunamimods;
 
 import org.apache.logging.log4j.Logger;
-//import com.stuunami.stuunamimods.proxy.ServerProxy;
+import com.stuunami.stuunamimods.Reference;
+import com.stuunami.stuunamimods.proxy.*;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -10,29 +11,29 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
  
-@Mod(modid = StuunamiMods.MODID, name = StuunamiMods.NAME, version = StuunamiMods.VERSION, acceptedMinecraftVersions = StuunamiMods.MC_VERSION)
+@Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION, acceptedMinecraftVersions = Reference.MC_VERSION)
 public class StuunamiMods {
- 
-    public static final String MODID = "stuunamimods";
-    public static final String NAME = "Stuunami Mods";
-    public static final String VERSION = "1.0";
-    public static final String MC_VERSION = "[1.12.2]";
+    
+    @SidedProxy(clientSide = Reference.CLIENT, serverSide = Reference.SERVER)
+    public static IProxy proxy;
  
     public static Logger logger;
  
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         logger = event.getModLog();
+        proxy.preInit(event);
     }
  
     @EventHandler
     public void init(FMLInitializationEvent event) {
         logger.info("TutorialMod.NAME " + "says hi!");
+        proxy.init(event);
     }
  
     @EventHandler
     public void postInit(FMLPostInitializationEvent event) {
- 
+    	proxy.postInit(event);
     }
  
 }
